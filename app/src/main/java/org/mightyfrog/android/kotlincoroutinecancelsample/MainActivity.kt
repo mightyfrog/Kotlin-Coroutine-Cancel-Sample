0ee71b9fccb9fea8c23b429bd729d7407738a15d
mightyfrog <mightyfrog.gc@gmail.com>
2018-09-07 00:24:39 +0900

    add clarification code

--- app/src/main/java/org/mightyfrog/android/kotlincoroutinecancelsample/MainActivity.kt
+++ app/src/main/java/org/mightyfrog/android/kotlincoroutinecancelsample/MainActivity.kt
@@ -1,19 +1,19 @@
 package org.mightyfrog.android.kotlincoroutinecancelsample
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import kotlinx.coroutines.experimental.DefaultDispatcher
-import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.withContext
 
 /**
  * @author Shigehiro Soejima
  */
+@SuppressLint("LogNotTimber")
 class MainActivity : AppCompatActivity() {
 //    private val rootJob = Job()
 //
@@ -34,23 +34,40 @@ class MainActivity : AppCompatActivity() {
 
     private var job: Job? = null
 
+    private val completionHandler = object : CompletionHandler {
+        override fun invoke(cause: Throwable?) {
+            Log.d("coroutine", "Completed: $cause")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener {
+            Log.d("coroutine", "Canceled")
             job?.cancel()
             Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show()
         }
 
-        job = launch(UI) {
+        job = launch(UI, onCompletion = completionHandler) {
             textView.text = withContext(DefaultDispatcher) { load() }
+            // lines after this will not be executed once the coroutine is canceled
+            Log.d("coroutine", "One")
+            Log.d("coroutine", "Two")
+            Log.d("coroutine", "Three")
+            textView.text = withContext(DefaultDispatcher) { load2() }
         }
     }
 
     private fun load(): String {
-        Thread.sleep(10 * 1000L)
+        Thread.sleep(5 * 1000L)
         return "Hello, Coroutine!"
+    }
+
+    private fun load2(): String {
+        Thread.sleep(5 * 1000L)
+        return "Hello, Coroutine again!"
     }
 }
